13f54221ed2bd9611989687cf741ec4880ded306
Sachin Malik <sachinmalik199821@gmail.com>
2022-02-09 16:01:38 +0530

    Add Link, Navlink, useNavigate

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import './App.css';
 import {Routes,Route} from 'react-router-dom'
 import Home from './components/Home';
 import About from './components/About';
+import Navbar from './components/Navbar';
+import OrderSummary from './components/OrderSummary';
+
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />}/>
-      <Route path='/about' element={<About />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />} />
+        <Route path='/order-summary' element={<OrderSummary/>}/>
+      </Routes>
+    </>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import {useNavigate} from 'react-router-dom';
+
 function Home() {
-  return <div>HOMEPAGE</div>;
+  const navigate=useNavigate();
+  return <>
+     <div>HOMEPAGE</div>
+     <button onClick={()=>navigate('order-summary',{replace:false})}>Place Order</button>
+  </>;
 }
 
 export default Home;
